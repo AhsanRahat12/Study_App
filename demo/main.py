533e64d2884d315accec74c163f9d4dc26fe2e36
AhsanRahat12 <s.rahatahsan@gmail.com>
2026-07-24 20:31:47 +0000

    style(We-just-created-a-simple-folder,-to-test-out-our-containerization-effort): We creted a temporary project directory with a flat structrure, intorduced a few changes in our main.py
    This is the beggining of our contianerization efforts, but done on the demo before the main file.

---
+++ demo/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from demo!")
+    print("Hello from demo!")
+    print("Hello from the demo, Lets get our hands dirty with some images!")
+
+if __name__ == "__main__":
+    main()
